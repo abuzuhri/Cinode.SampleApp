533a4d52fea0098045355753bb8706d1b105ac94
Tareq Abuzuhri <tareq_abuzuhri@hotmail.com>
2017-04-05 19:21:41 +0200

    add context

--- Cinode.RateApp.Data/Context/MainContext.cs
+++ Cinode.RateApp.Data/Context/MainContext.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Cinode.RateApp.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Cinode.RateApp.Data.Context
 {
-    class MainContext
+    public class MainContext
     {
+        public DbSet<Rate> Rates { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
     }
 }
