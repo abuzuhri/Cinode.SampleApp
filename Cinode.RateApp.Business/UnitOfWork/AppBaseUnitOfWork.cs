9cc82c89097e13792388cdbe25e64b4b51190a9d
Tareq Abuzuhri <tareq_abuzuhri@hotmail.com>
2017-04-05 19:27:09 +0200

    add unit of work

---
+++ Cinode.RateApp.Business/UnitOfWork/AppBaseUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Cinode.RateApp.Data.Context;
+using Cinode.RateApp.Data.Entity;
+using GoTech.Framework.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinode.RateApp.Business.UnitOfWork
+{
+    public class AppBaseUnitOfWork : BaseGoTechUnitOfWork
+    {
+        public BaseDataRepository<Rate> RateRepository { get; set; }
+        public BaseDataRepository<Skill> SkillRepository { get; set; }
+        public BaseDataRepository<User> UserRepository { get; set; }
+        public BaseDataRepository<UserSkill> UserSkillRepository { get; set; }
+
+        public AppBaseUnitOfWork() : base(new MainContext())
+        {
+            RateRepository = this.Repository<Rate>();
+            SkillRepository = this.Repository<Skill>();
+            UserRepository = this.Repository<User>();
+            UserSkillRepository = this.Repository<UserSkill>();
+        }
+    }
+}
